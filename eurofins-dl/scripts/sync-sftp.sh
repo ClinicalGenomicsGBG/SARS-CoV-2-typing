8fb0c85ff5bac2dc67375df14cf1870629bb2231
Anders Lind <mail@anderslind.se>
2021-03-03 08:46:18 +0100

    Commented out the pangolin renaming part

--- eurofins-dl/scripts/sync-sftp.sh
+++ eurofins-dl/scripts/sync-sftp.sh
@@ -6,7 +6,7 @@ if [ "$#" -lt 2 ]; then
 fi
 
 LOGFILE='/medstore/logs/pipeline_logfiles/sars-cov-2-typing/eurofins-dl.log'
-DATALOC='/medstore/results/clinical/SARS-CoV-2-typing/eurofins_data/'
+DATALOC='/medstore/results/clinical/SARS-CoV-2-typing/eurofins_data/goteborg/'
 CURRENTTIME=$(date "+%Y-%m-%d %H:%M:%S")
 FTPUSER=$1
 FTPKEY=$2
@@ -17,7 +17,7 @@ echo "** LOG: Starting sFTP sync @ $CURRENTTIME" >> $LOGFILE
 # Sync the sFTP
 # Uses lftp. Now produces quite a lot of log, one line per file
 # Can be lowered be setting -vv or -v
-lftp -e 'mirror -vvv / /medstore/results/clinical/SARS-CoV-2-typing/eurofins_data/; bye' \
+lftp -e 'mirror -vvv / /medstore/results/clinical/SARS-CoV-2-typing/eurofins_data/goteborg; bye' \
   -u $FTPUSER,$FTPKEY \
   ftp.gatc-biotech.com:21 \
   &>> $LOGFILE
@@ -38,22 +38,23 @@ find $DATALOC -name "md5sums.txt" -newerct "$CURRENTTIME" | while read x; do \
 done
 cd $CURDIR
 
-# Rename pangolin file to have a unique identifier (same as plate name)
-echo "** LOG: Making renamed copy of pangolin file." >> $LOGFILE
-find $DATALOC -name "CO*_pangolin_lineage_classification.txt" -newerct "$CURRENTTIME" | while read x; do \
-  echo "** LOG: found un-renamed pangolin file $x" >> $LOGFILE
-  FILENAME=$(basename $x)
-  BASE=${x%/$FILENAME}
-  PLATEID=${BASE##*/}
-  # This makes a copy Change to mv if we don't need original
-  new_x=${BASE}/${PLATEID}_$FILENAME
-  echo "** LOG: Renaming to $new_x" >> $LOGFILE
-  cp $x $new_x &>> $LOGFILE
-  EXITSTATUS=$?
-  if [ $EXITSTATUS -ne 0 ]; then
-    echo "** ERROR: Problem when copying file $x" >> $LOGFILE
-  fi
-done
+# This section is commented out as it is now being done by a python script
+# # Rename pangolin file to have a unique identifier (same as plate name)
+# echo "** LOG: Making renamed copy of pangolin file." >> $LOGFILE
+# find $DATALOC -name "CO*_pangolin_lineage_classification.txt" -newerct "$CURRENTTIME" | while read x; do \
+#   echo "** LOG: found un-renamed pangolin file $x" >> $LOGFILE
+#   FILENAME=$(basename $x)
+#   BASE=${x%/$FILENAME}
+#   PLATEID=${BASE##*/}
+#   # This makes a copy Change to mv if we don't need original
+#   new_x=${BASE}/${PLATEID}_$FILENAME
+#   echo "** LOG: Renaming to $new_x" >> $LOGFILE
+#   cp $x $new_x &>> $LOGFILE
+#   EXITSTATUS=$?
+#   if [ $EXITSTATUS -ne 0 ]; then
+#     echo "** ERROR: Problem when copying file $x" >> $LOGFILE
+#   fi
+# done
 
 #End log
 CURRENTTIME=$(date "+%Y-%m-%d %H:%M:%S")
