a4215e979ada34e65ab1fdbacaeba75f99f9e79c
Vilma Canfjorden <vilma.canfjorden@gu.se>
2021-03-03 08:40:46 +0100

    added unique names to output file (name of directory, plate)

--- pangolin_fillemptyfield.py
+++ pangolin_fillemptyfield.py
@@ -5,6 +5,7 @@ import pandas as pd
 import os
 import datetime as dt
 import fnmatch
+import glob
 
 def arg():
     parser = argparse.ArgumentParser(prog="pangolin_fillemptyfield.py")
@@ -16,25 +17,25 @@ def arg():
 def check_files():
     now = dt.datetime.now()
     ago = now-dt.timedelta(minutes=1440)
+#print(os.path.basename(os.path.dirname(path)))
 
     path_list = []
-    for root, dirs,files in os.walk('/home/xcanfv/GBG_sars-cov-2-typing/sars-cov-2-typing/'):
-        for fname in files:
-            path = os.path.join(root, fname)
-            st = os.stat(path)
-            mtime = dt.datetime.fromtimestamp(st.st_mtime)
-            if mtime > ago:
-               # print('%s modified %s'%(path, mtime))
-                path_list.append(path)
-        return path_list
+    #for path in glob.glob('/medstore/results/clinical/SARS-CoV-2-typing/eurofins_data/goteborg/2021*/*', recursive=True):
+    for path in glob.glob('/home/xcanfv/GBG_sars-cov-2-typing/sars-cov-2-typing/*', recursive=True):
+        st = os.stat(path)
+        mtime = dt.datetime.fromtimestamp(st.st_mtime)
+        if mtime > ago:
+            #print('%s modified %s'%(path, mtime))
+            path_list.append(path)
+    return path_list
 
 
 def automatic(path):
     for f in path:
-        if fnmatch.fnmatch(os.path.basename(f), "2021-*_pangolin_lineage_classification.txt"):
+        if fnmatch.fnmatch(os.path.basename(f), "*_pangolin_lineage_classification.txt"):
             print("updating: " + f)
             df = pd.DataFrame(pd.read_csv(f, sep="\t")).fillna(value = "NULL")
-            df.to_csv(os.path.basename(f).replace(".txt","_fillempty.txt"), index=None, header=True, sep="\t")
+            df.to_csv(os.path.basename(os.path.dirname(f))+"_"+os.path.basename(f).replace(".txt","_fillempty.txt"), index=None, header=True, sep="\t")
 
 
 def fill_empty_cells(args):
