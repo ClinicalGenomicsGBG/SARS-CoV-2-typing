5568a42af7d1f10988ee91374bbeb8325347155c
Vilma Canfjorden <vilma.canfjorden@gu.se>
2021-03-08 14:02:58 +0100

    fixed direkttest flag

--- direkttest_cronscript.sh
+++ direkttest_cronscript.sh
@@ -7,4 +7,4 @@ source activate vilma_general
 /apps/bio/repos/sars-cov-2-typing/direkttest_csv.py -a
 
 # Upload files to gms bucket, checks files automatically 24 hrs
-/apps/bio/repos/sars-cov-2-typing/hcp_covid.py -b goteborg -ep https://vgtn0008.hcp1.vgregion.se:443 -aki Z290ZWJvcmc= -sak 50b94f5526cc530057af9694cf09a870 -e 
+/apps/bio/repos/sars-cov-2-typing/hcp_covid.py -b goteborg -ep https://vgtn0008.hcp1.vgregion.se:443 -aki Z290ZWJvcmc= -sak 50b94f5526cc530057af9694cf09a870 -d 
