cd1b52af0ae2a960753d62511ee2f787880c1cf2
Vilma Canfjorden <vilma.canfjorden@gu.se>
2021-03-24 15:21:44 +0100

    added module for checking files automatcially every 24 hrs

---
+++ tools/check_files.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import datetime as dt
+import glob
+import os
+
+
+# Check files automatically (24hrs)
+def check_files(file_path):
+    now = dt.datetime.now()
+    ago = now-dt.timedelta(minutes=1440)
+
+    path_list = []
+    for path in glob.glob(file_path, recursive=True):
+        st = os.stat(path)
+        mtime = dt.datetime.fromtimestamp(st.st_ctime) # ctime for time of change of file
+        if mtime > ago:
+            #print('%s modified %s'%(path, mtime))
+            path_list.append(path)
+
+    return path_list
+
+
