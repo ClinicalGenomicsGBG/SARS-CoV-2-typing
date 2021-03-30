029ebf20e121de3fd5c5d2fbf1d870bffe278be1
Anders Lind <mail@anderslind.se>
2021-03-30 14:15:02 +0200

    Updated who gets an e-mail

--- tools/emailer.py
+++ tools/emailer.py
@@ -30,7 +30,8 @@ def email_micro(subject, body):
 
     msg['Subject'] = f'{subject}'
     msg['From'] = "clinicalgenomics@gu.se"
-    msg['To'] = "johan.ringlander@vgregion.se"
+    msg['To'] = ["johan.ringlander@vgregion.se", "josefin.olausson@vgregion.se"]
+    msg['Cc'] = "anders.lind.cgg@gu.se"
 
     #Send the messege
     s = smtplib.SMTP('smtp.gu.se')
