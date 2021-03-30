ae5718a80fac0c0ef1989eb64bca957b29eedc89
Vilma Canfjorden <vilma.canfjorden@gu.se>
2021-03-30 16:22:37 +0200

    added check for file on HCP before trying to upload

--- nextseq_cronscript.py
+++ nextseq_cronscript.py
@@ -111,11 +111,14 @@ def clc_sync(password, run):
 # Upload files and json to selected bucket on HCP.
 def upload_fastq(hcp_paths,hcpm,logger):
     for file_pg in hcp_paths:
-        try:
-            hcpm.upload_file(file_pg, "covid-wgs/"+os.path.basename(file_pg))
-            logger.info(f"uploading: {file_pg}")
-        except Exception as e:
-            logger.error(e)
+        if hcpm.search_objects(file_pg) is None:
+            try:
+                hcpm.upload_file(file_pg, "covid-wgs/"+os.path.basename(file_pg))
+                logger.info(f"uploading: {file_pg}")
+            except Exception as e:
+                logger.error(e)
+                continue
+        else:
             continue
 
 
