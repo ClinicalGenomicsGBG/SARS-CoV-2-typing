19c1eadf7a3f577505b7d216f240c853c81441c2
Jens P Werling <jens.p.werling@gmail.com>
2021-06-10 14:12:55 +0200

    Add basic plugin and about html
    The about html is rendered on the torrent server at the plugin overview interface.

---
+++ microbiology-plugin/covid_seqstore_transfer/covid_seqstore_transfer.py
@@ -0,0 +1,10 @@
+from ion.plugin import IonPlugin, RunType, RunLevel
+
+
+class covid_seqstore_transfer(IonPlugin):
+    version = "0.0.1.0"
+    runtypes = [RunType.COMPOSITE]
+    runlevel = [RunLevel.LAST]
+
+    def launch():
+        pass
