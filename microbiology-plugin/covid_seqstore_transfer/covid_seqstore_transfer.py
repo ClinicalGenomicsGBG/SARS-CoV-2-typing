c3036eacedb53bb851e0dd3bcf04252e80092640
Jens P Werling <jens.p.werling@gmail.com>
2021-06-10 14:41:55 +0200

    Add function for cleaning sample names of typos

--- microbiology-plugin/covid_seqstore_transfer/covid_seqstore_transfer.py
+++ microbiology-plugin/covid_seqstore_transfer/covid_seqstore_transfer.py
@@ -1,10 +1,22 @@
 from ion.plugin import IonPlugin, RunType, RunLevel
 
 
+def clean_samplename(sample_name):
+    """Return clean and formatted samplename in the event of typos."""
+    for character in [' ']:
+        sample_name.replace(character, '')
+
+    for character in ['_']:
+        sample_name.replace(character, '-')
+
+    return sample_name
+
+
 class covid_seqstore_transfer(IonPlugin):
     version = "0.0.1.0"
     runtypes = [RunType.COMPOSITE]
     runlevel = [RunLevel.LAST]
 
     def launch(self):
-        pass
+        for sample_name, barcode_info in self.startplugin['plan']['barcodedSamples'].items():
+            sample_name = clean_samplename(sample_name)
