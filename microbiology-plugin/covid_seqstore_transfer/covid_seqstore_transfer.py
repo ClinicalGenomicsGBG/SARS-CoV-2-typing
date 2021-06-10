6fc5564fb1f3860b118268ff711bae1d8d48b44a
Jens P Werling <jens.p.werling@gmail.com>
2021-06-10 14:54:02 +0200

    Add filter for non covid samples
    Simple regex based on limited availability of sample names.

--- microbiology-plugin/covid_seqstore_transfer/covid_seqstore_transfer.py
+++ microbiology-plugin/covid_seqstore_transfer/covid_seqstore_transfer.py
@@ -1,3 +1,5 @@
+import re
+
 from ion.plugin import IonPlugin, RunType, RunLevel
 
 
@@ -12,11 +14,21 @@ def clean_samplename(sample_name):
     return sample_name
 
 
+def is_covid_sample(sample_name):
+    return re.search('D[A-Z]2[0-9]-[0-9]+', sample_name, re.IGNORECASE)
+
+
 class covid_seqstore_transfer(IonPlugin):
     version = "0.0.1.0"
     runtypes = [RunType.COMPOSITE]
     runlevel = [RunLevel.LAST]
 
     def launch(self):
+        not_processed_samples = []
+
         for sample_name, barcode_info in self.startplugin['plan']['barcodedSamples'].items():
             sample_name = clean_samplename(sample_name)
+
+            if not is_covid_sample(sample_name):
+                not_processed_samples.append(sample_name)
+                continue
